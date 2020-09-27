bcba0e01f36f8658e0d5a2d89903d1711471483e
Mike Williamson <michael@dikuw.com>
2020-09-27 10:40:50 -0700

    added Sanity plugin

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,7 +1,23 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
   siteMetadata: {
-    title: 'Slick\'s Slices',
+    title: 'Slicks Slices',
     siteUrl: 'https://gatsby.pizza',
     description: 'The best pizza place in Hanoi',
-  }
-}
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'an3csoxy',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.STANITY_TOKEN,
+      },
+    },
+  ]
+};
