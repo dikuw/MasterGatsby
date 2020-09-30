34cc725665e3712714b36115d671c65ea948f6fc
Mike Williamson <michael@dikuw.com>
2020-09-30 16:51:56 -0700

    added pagination

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import Pagination from '../components/Pagination';
 
 const SlicemasterGrid = styled.div`
   display: grid;
@@ -36,10 +37,17 @@ const Slicemaster = styled.div`
   }
 `;
 
-export default function SliceMastersPage({ data }) {
+export default function SliceMastersPage({ data, pageContext }) {
   const slicemasters = data.slicemasters.nodes;
   return (
     <>
+      <Pagination 
+        pageSize={parseInt(process.env.GATSBY_PAGE_SIZE)} 
+        totalCount={data.slicemasters.totalCount} 
+        currentPage={pageContext.currentPage || 1} 
+        skip={pageContext.skip} 
+        base="/slicemasters"
+      />
       <SlicemasterGrid>
         {slicemasters.map(person => (
           <Slicemaster key={person.id}>
@@ -60,7 +68,7 @@ export default function SliceMastersPage({ data }) {
 }
 
 export const query = graphql`
-  query($skip: Int = 0, $pageSize: Int = 4) {
+  query($skip: Int = 0, $pageSize: Int = 2) {
     slicemasters: allSanityPerson(limit: $pageSize, skip: $skip) {
       totalCount
       nodes {
