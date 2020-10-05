e7c43069e35a0d831262398bd916e2758b7b47e6
Mike Williamson <michael@dikuw.com>
2020-10-05 10:47:32 -0700

    adding form

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
 
 export default function OrderPage() {
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+  });
   return (
     <>
-      <SEO
-        title={'Order a pizza'}
-      />
-      <p>Orders Page</p>
+      <SEO title={'Order a pizza'} />
+      <form>
+        <fieldset>
+          <legend>Your Info</legend>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" value={values.name} onChange={updateValue}></input>
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" value={values.email} onChange={updateValue}></input>
+        </fieldset>
+        <fieldset>
+          <legend>Menu</legend>
+        </fieldset>
+        <fieldset>
+          <legend>Order</legend>
+        </fieldset>
+      </form>
     </>
   );
 }
