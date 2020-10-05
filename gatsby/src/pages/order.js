29ab3db65e18bd05385a0dd9b2482e221a24a2c9
Mike Williamson <michael@dikuw.com>
2020-10-05 13:48:56 -0700

    add pizzas

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import SEO from '../components/SEO';
 import useForm from '../utils/useForm';
+import calcPizzaPrice from '../utils/calcPizzaPrice';
+import formatMoney from '../utils/formatMoney';
 
-export default function OrderPage() {
+export default function OrderPage({ data }) {
   const { values, updateValue } = useForm({
     name: '',
     email: '',
   });
+  const pizzas = data.pizzas.nodes;
   return (
     <>
       <SEO title={'Order a pizza'} />
@@ -20,6 +25,21 @@ export default function OrderPage() {
         </fieldset>
         <fieldset>
           <legend>Menu</legend>
+          {pizzas.map(pizza => (
+            <div key={pizza.id}>
+              <Img width="50" height="50" fluid={pizza.image.asset.fluid} alt={pizza.name} />
+              <div>
+                <h2>{pizza.name}</h2>
+              </div>
+              <div>
+                {['S', 'M', 'L'].map(size => (
+                  <button key={pizza.id + size} type="button">
+                    {size} {formatMoney(calcPizzaPrice(pizza.price, size))}
+                  </button>
+                ))}
+              </div>
+            </div>
+          ))}
         </fieldset>
         <fieldset>
           <legend>Order</legend>
@@ -28,3 +48,26 @@ export default function OrderPage() {
     </>
   );
 }
+
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        price
+        image {
+          asset {
+            fluid(maxWidth: 100) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
