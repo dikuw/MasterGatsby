065e5cc766665e9ad17d829b44d54788e0c5a7f9
Mike Williamson <michael@dikuw.com>
2020-10-05 15:46:21 -0700

    add orders

---
+++ gatsby/src/components/Order.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import MenuItemStyles from '../styles/MenuItemStyles';
+import Img from 'gatsby-image';
+import calcPizzaPrice from '../utils/calcPizzaPrice';
+import formatMoney from '../utils/formatMoney';
+
+export default function Order({ order, pizzas, removeFromOrder, }) {
+  return (
+    <>
+      {order.map((item, index) => {
+        const pizza = pizzas.find(pizza => pizza.id === item.id);
+        return (
+          <MenuItemStyles key={item.id}>
+            <Img fluid={pizza.image.asset.fluid} />
+            <h2>{pizza.name}</h2>
+            <p>
+              {formatMoney(calcPizzaPrice(pizza.price, item.size))}
+              <button 
+                type="button" 
+                className="remove" 
+                title={`Remove ${item.size} ${pizza.name} from Order`}
+                onClick={() => removeFromOrder(index)}
+              >
+                &times;
+              </button>
+            </p>
+          </MenuItemStyles>
+        )
+      })}
+    </>
+  )
+}
--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -2,18 +2,24 @@ import React, { useState } from 'react';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import SEO from '../components/SEO';
+import Order from '../components/Order';
 import useForm from '../utils/useForm';
+import usePizza from '../utils/usePizza';
 import calcPizzaPrice from '../utils/calcPizzaPrice';
 import formatMoney from '../utils/formatMoney';
 import OrderStyles from '../styles/OrderStyles';
 import MenuItemStyles from '../styles/MenuItemStyles';
 
 export default function OrderPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   const { values, updateValue } = useForm({
     name: '',
     email: '',
   });
-  const pizzas = data.pizzas.nodes;
+  const { order, addToOrder, removeFromOrder } = usePizza({
+    pizzas,
+    inputs: values,
+  });
   return (
     <>
       <SEO title={'Order a pizza'} />
@@ -35,7 +41,10 @@ export default function OrderPage({ data }) {
               </div>
               <div>
                 {['S', 'M', 'L'].map(size => (
-                  <button key={pizza.id + size} type="button">
+                  <button key={pizza.id + size} type="button" onClick={() => addToOrder({
+                    id: pizza.id,
+                    size,
+                  })}>
                     {size} {formatMoney(calcPizzaPrice(pizza.price, size))}
                   </button>
                 ))}
@@ -45,6 +54,7 @@ export default function OrderPage({ data }) {
         </fieldset>
         <fieldset className="order">
           <legend>Order</legend>
+          <Order order={order} pizzas={pizzas} removeFromOrder={removeFromOrder} />
         </fieldset>
       </OrderStyles>
     </>
